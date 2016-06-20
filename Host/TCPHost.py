78a4467e7b8ecc58245fdbf472efbce0ca4bfa97
Steve Withers <SteveWithers3@gmail.com>
2016-06-20 11:09:51 +0000

    TCPHost now reconnect automatically

--- Host/TCPHost.py
+++ Host/TCPHost.py
@@ -1,21 +1,34 @@
 import socket
+import time
 
-TCP_IP = '127.0.0.1'
+TCP_IP = '10.0.0.1'
 TCP_PORT = 5005
 BUFFER_SIZE = 1024
 
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.bind((TCP_IP,TCP_PORT))
 
-s.listen(1)
+running = True
+connecting = True
 
-conn, addr = s.accept()
-print "Connecttion from", addr
+while running:
 
-while 1:
-    data = conn.recv(BUFFER_SIZE)
-    if not data: break
-    print "received data",data
+    if connecting:
+        print "listening"
+        s.listen(1)
 
+        conn, addr = s.accept()
+        print "Connecttion from", addr
+        connecting = False
+    else:
 
+        while 1:
+            data = conn.recv(BUFFER_SIZE)
+            if not data:
+                connecting = True
+                break
+            print "received data",data
+            time.sleep(0.5)
+
+print "connection closed"
 conn.close()
