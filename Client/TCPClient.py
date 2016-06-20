fcd691c31296d90794081a136798bc2c309f9e53
Steve Withers <SteveWithers3@gmail.com>
2016-06-20 11:18:44 +0000

    Added LED indicator

--- Client/TCPClient.py
+++ Client/TCPClient.py
@@ -1,11 +1,15 @@
 import socket
-import time 
+import time
+import pigpio
 
 class TCPSend:
     def __init__(self):
+        self.pi = pigpio.pi('localhost')
         self.TCP_IP = '10.0.0.1'
         self.TCP_PORT = 5005
         self.BUFFERSIZE = 1024
+
+        self.LED_PIN = 21
 
         self.socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         
@@ -13,10 +17,12 @@ class TCPSend:
 
     def test(self):
         self.socket.connect((self.TCP_IP, self.TCP_PORT))
+        self.pi.write(self.LED_PIN,1)
         self.socket.send("test")
         time.sleep(0.5)
         self.socket.send("test")
         self.socket.close()
+        self.pi.write(self.LED_PIN,0)
 
         print "done"
 
