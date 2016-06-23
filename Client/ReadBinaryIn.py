548315958a45b192b826813652d514a0f9c26ffa
Steve Withers <SteveWithers3@gmail.com>
2016-06-23 14:50:52 +0000

    Added pipeline method instead of polling to speed up transmitting

--- Client/ReadBinaryIn.py
+++ Client/ReadBinaryIn.py
@@ -1,5 +1,6 @@
 import pigpio
 from TCPClient import TCPSend
+import time
 
 pi = pigpio.pi('localhost')
 TCPSend = TCPSend(pi)
@@ -7,27 +8,38 @@ TCPSend = TCPSend(pi)
 numberPins = [21,20,26,16,19,13,12,6,5,7]
 addressPins = [8,11,25,9]
 validPin = 10
+readyPin = 24
 
 address = 0
 number = 0
 
 values = [0,0,0,0,0,0,0,0]
 
+pi.write(readyPin,0)
+
 while True:
-    address = 0
-    if pi.read(validPin):
-        for i in range(len(addressPins)):
-            address = address + (pi.read(addressPins[i])*pow(2,i))
 
-        number = 0
-        for i in range(len(numberPins)):
-            number = number + (pi.read(numberPins[i])*pow(2,i))
+    while not pi.read(validPin):
+        pass
+ 
+    address = 0  
+    for i in range(len(addressPins)):
+        address = address + (pi.read(addressPins[i])*pow(2,i))
+
+    number = 0
+    for i in range(len(numberPins)):
+        number = number + (pi.read(numberPins[i])*pow(2,i))
             
-        if pi.read(validPin):
-            values[address] = number
-            TCPSend.sendData(address,number)
-        
+    values[address] = number
+    TCPSend.sendData(address,number)
 
-    
+    #set ready signal
+    pi.write(readyPin,1)
+
+    #wait for valid signal rising edge
+    while pi.read(validPin):
+        pass
+    #reset ready pin
+    pi.write(readyPin,0)
         
         
