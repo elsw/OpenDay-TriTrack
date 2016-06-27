04f0c36723e8d74479e7440a02c74dcd22a32ee6
Steve Withers <SteveWithers3@gmail.com>
2016-06-27 12:41:30 +0000

    Sorted out timeout procedure for unexpected host drop

--- Client/TCPClient.py
+++ Client/TCPClient.py
@@ -22,32 +22,18 @@ class TCPSend:
 
         except:
             try:
+                self.pi.write(self.LED_PIN,0)
                 self.socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
                 self.socket.connect((self.TCP_IP, self.TCP_PORT))
+                self.socket.settimeout(5)
                 self.pi.write(self.LED_PIN,1)
                 print "connected"
                 self.discNotice = False
             except:
+                "disconnected"
                 if not self.discNotice:
                     print "disconnected"
                     self.discNotice = True
                 time.sleep(0.5)
-                self.pi.write(self.LED_PIN,0)
+                
 
-    def test(self):
-        self.socket.connect((self.TCP_IP, self.TCP_PORT))
-        self.pi.write(self.LED_PIN,1)
-        self.socket.send("test")
-        time.sleep(0.5)
-        self.socket.send("test")
-        self.socket.close()
-        self.pi.write(self.LED_PIN,0)
-
-        print "done"
-    
-
-
-
-if __name__ == "__main__":
-    t = TCPSend(pigpio.pi('localhost'))
-    t.test()
